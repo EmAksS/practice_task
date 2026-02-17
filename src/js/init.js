5b8441289a8c2b04c2acf77edcd46a9358985011
EmAksS <emirshuman1999@gmail.com>
2026-02-17 18:24:48 +0300

    Вторая страница. Реализация отдельного скрипта инициализации. Добавление ссылок в header

---
+++ src/js/init.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isChromium = userAgent.includes('chrome') || userAgent.includes('edg') || userAgent.includes('opr') || userAgent.includes('opera');
+    
+    if (!isChromium) {
+        document.documentElement.classList.add('no-liquid-glass-support');
+        const elements = document.querySelectorAll('.header--glass');
+        elements.forEach(el => {
+            el.classList.add('liquid-glass-fallback');
+        });
+    }
+    else{
+        const elements = document.querySelectorAll('.header--glass');
+        elements.forEach(el => {
+            el.classList.add('liquid-glass-apply');
+        });
+    }
+
+    const currentPage = "./" + window.location.pathname.split("/").pop();
+    console.log(currentPage);
+
+    $(".header__nav__element a").each(function () {
+        console.log($(this).attr("href"));
+        if ($(this).attr("href") === (currentPage)) {
+            $(this).parent().addClass("header__nav__element--active");
+        }
+    });
+});
